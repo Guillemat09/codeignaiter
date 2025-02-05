82be446d281affe3ae2dda9e3c0a6aea5aea6e59
Guillemat09 <guillemateos09@gmail.com>
2025-02-05 12:45:24 +0100

    mas cambios

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('users' , 'UserController'::index); //Listar usuarios 
+$routes->get('users/save', '  UserController'::saveUser);
+$routes->get('users/save/(:num)', '  UserController'::saveUser/$1);
+$routes->post('users/save', '  UserController'::saveUser);
+$routes->post('users/save/(:num)', '  UserController'::saveUser/$1);
+$routes->get('users/delete/(:num)', '  UserController'::delete/$1);
