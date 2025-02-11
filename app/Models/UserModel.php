edc38d3ab6360dcd1c7626bfc2941ac79aac979e
Guillermo <guillemateos09@alumnos.alborfp.com>
2025-02-11 18:58:48 +0100

    cambios

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -1,13 +1,13 @@
 <?php
+
 namespace App\Models;
 
 use CodeIgniter\Model;
 
-class UserModel extends Model 
+class UserModel extends Model
 {
-    protected $table = 'users';
-
-    protected $primaryKey = 'id';
-
-    protected $allowedFields = ['name', 'email'];
+    protected $table      = 'usuarios';  // La tabla en la base de datos
+    protected $primaryKey = 'id';  // La clave primaria
+    protected $allowedFields = ['name', 'email'];  // Campos que se pueden insertar/actualizar
+    protected $useTimestamps = true;  // Usar marcas de tiempo
 }
